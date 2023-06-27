fadcd5b39668a42ffc646c771e7fc7ab5a2f0f45
Nguyen Minh Tuan <tnowad@gmail.com>
2023-06-27 11:36:58 +0700

    feat(animation): add animation slide

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,12 +1,19 @@
 import { useLocation } from 'react-router-dom';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import './Layout.css';
 import { Header } from './Header';
-export function Layout({ ...prop }) {
+
+export function Layout({ ...props }) {
   const location = useLocation();
 
   return (
     <div>
       <Header />
-      <div>{prop.children}</div>
+      <TransitionGroup>
+        <CSSTransition key={location.key} classNames="slide" timeout={300}>
+          <div className="page">{props.children}</div>
+        </CSSTransition>
+      </TransitionGroup>
     </div>
   );
 }
