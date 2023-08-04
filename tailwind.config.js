2abde4f7b026ff8732d4ae7119dbb434fe25081f
Nguyen Minh Tuan <tnowad@gmail.com>
2023-08-04 13:45:11 +0700

    feat(tailwind): add custom colors and enable dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,35 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        black: "#0D0D0D",
+        "back/50": "rgba(21, 21, 21, 0.5)",
+        white: "#FFFFFF",
+        "white/10": "rgba(255, 255, 255, 0.1)",
+        "white/25": "rgba(255, 255, 255, 0.25)",
+        "white/70": "rgba(255, 255, 255, 0.7)",
+        onyx: "#181818",
+        "gray-dark": "#808080",
+        "gray-medium": "rgba(128, 128, 128, 0.5)",
+        "gray-light": "#C5C5C5",
+        "blue-light": "rgba(63, 100, 234, 0.43)",
+        "red-light": "rgba(233, 63, 64, 0.43)",
+        "yellow-light": "rgba(255, 184, 0, 0.43)",
+        "card-border": {
+          dark: "#383737",
+          light: "#EBEBEB",
+        },
+        "white-bg": "#FBFBFB",
+        "white-faint": "#F3F3F3",
       },
     },
   },
+  darkMode: "class",
   plugins: [],
-}
+};
